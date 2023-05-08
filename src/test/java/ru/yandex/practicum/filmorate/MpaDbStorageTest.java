fb178864687857d88b00c60faa93de66cd9f2f3c
Freddycs14 <freddycs14@gmail.com>
2023-05-08 16:11:54 +0300

    fix: поправил код

--- src/test/java/ru/yandex/practicum/filmorate/MpaDbStorageTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/MpaDbStorageTest.java
@@ -1,14 +1,11 @@
 package ru.yandex.practicum.filmorate;
 
 import lombok.RequiredArgsConstructor;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.annotation.DirtiesContext;
-import org.springframework.test.jdbc.JdbcTestUtils;
 import ru.yandex.practicum.filmorate.model.Mpa;
 import ru.yandex.practicum.filmorate.storage.mpa.MpaDbStorage;
 
