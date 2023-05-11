ed920b87fc82558354925353f02655aba08bc085
Freddycs14 <freddycs14@gmail.com>
2023-05-11 23:18:01 +0300

    fix: поправил замечания после проверки

--- src/main/java/ru/yandex/practicum/filmorate/extractor/GenreResultSetExtractor.java
+++ src/main/java/ru/yandex/practicum/filmorate/extractor/GenreResultSetExtractor.java
@@ -1,14 +1,12 @@
 package ru.yandex.practicum.filmorate.extractor;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.core.ResultSetExtractor;
 import org.springframework.stereotype.Component;
 import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.model.Genre;
 import ru.yandex.practicum.filmorate.model.Mpa;
-import ru.yandex.practicum.filmorate.storage.mpa.MpaStorage;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
